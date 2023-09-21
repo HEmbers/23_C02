e8628ef90d9a47a87fc2e8036b8db139da4f5a0d
HEmbers <kpmbusd@gmail.com>
2023-09-21 18:02:44 +0900

    monte carlo

--- 230921/230921/lecture4.cpp
+++ 230921/230921/lecture4.cpp
@@ -3,13 +3,14 @@
 #include<time.h>
 #include<math.h>
 
+void bar(double now, double total);
+
 int main(void)
 {
-	int per = 100;
 	double x = 0, y = 0, count = 0, inside = 0;
 	srand(time(NULL));
 
-	while (count < 10000)
+	while (count < 100000)
 	{
 		x = (double)rand() / (double)RAND_MAX;
 		y = (double)rand() / (double)RAND_MAX;
@@ -17,14 +18,21 @@ int main(void)
 
 		if ((x * x) + (y * y) <= 1)
 			inside++;
-		printf("%.2lf%%진행... 원주율 %.7lf", count / 100, (inside / count) * 4);
-		for (int a = 0; a < per; a += 5) {
-			printf("■");
-		}
-		for (int b = 100; b >=per; b -= 5) {
-			printf("□");
-		}
+		printf("%.2lf%%진행... 원주율 %.7lf", count / 1000, (inside / count) * 4);
+		bar(count, 100000);
 		printf("\n");
 	}
 	return 0;
+}
+
+void bar(double now, double total) {
+	int length = 20;
+	int completed = now / total * length;
+	
+		for (int i = 0; i < length; i++) {
+			if (i < completed)
+				printf("■");
+			else
+				printf("□");
+		}
 }
