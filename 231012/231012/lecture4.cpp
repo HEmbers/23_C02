8a055993ba6547eb3b43486a7e0b3e2cb98daf1e
HEmbers <kpmbusd@gmail.com>
2023-10-12 15:38:10 +0900

    swap

---
+++ 231012/231012/lecture4.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void swap(int *pa, int*pb);
+
+int main()
+{
+	int a = 10, b=500;
+	printf("%d %d", a, b);
+	swap(&a, &b);
+	printf("%d %d", a, b);
+}
+
+void swap(int *pa, int*pb) {
+	int p;
+	p = *pa;
+	*pa = *pb;
+	*pb = p;
+}
